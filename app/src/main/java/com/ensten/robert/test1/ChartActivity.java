221b72193562d932742adfff102bed762fb28462
Robert <Robert>
2017-04-05 09:47:48 +0200

    Initial commit

---
+++ app/src/main/java/com/ensten/robert/test1/ChartActivity.java
@@ -0,0 +1,17 @@
+package com.ensten.robert.test1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Robert on 03/04/2017.
+ */
+
+public class ChartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart_activity);
+
+    }
+}
